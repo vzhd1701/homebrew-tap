265e527a97e65191b313bb9b2523023c6ea18f09
Vlad <vzhd1701@gmail.com>
2022-06-20 17:37:00 +0500

    update install script

--- Formula/enex2notion.rb
+++ Formula/enex2notion.rb
@@ -15,7 +15,18 @@ class Enex2notion < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "1acdeccf2213e5e3011b60ebf02f5b4fbf153e049990744bd79bcee84fffb0f7"
   end
 
+  depends_on "freetype" => :build
+  depends_on "swig" => :build
+  depends_on "mupdf"
   depends_on "python@3.10"
+
+  on_linux do
+    depends_on "harfbuzz"
+    depends_on "mujs"
+    depends_on "openjpeg"
+    depends_on "gumbo-parser"
+    depends_on "jbig2dec"
+  end
 
   resource "beautifulsoup4" do
     url "https://files.pythonhosted.org/packages/e8/b0/cd2b968000577ec5ce6c741a54d846dfa402372369b8b6861720aa9ecea7/beautifulsoup4-4.11.1.tar.gz"
@@ -143,7 +154,28 @@ class Enex2notion < Formula
   end
 
   def install
-    virtualenv_install_with_resources
+    if OS.linux?
+      pymupdf_dirs = {
+        include_dirs: [
+          Formula["mupdf"].include/"mupdf",
+          Formula["freetype2"].include/"freetype2",
+        ],
+        library_dirs: [lib],
+      }
+      (buildpath/"pymupdf_dirs.env").write(pymupdf_dirs.to_json)
+      ENV["PYMUPDF_DIRS"] = File.expand_path("pymupdf_dirs.env")
+    end
+
+    ENV["PYMUPDF_SETUP_MUPDF_BUILD"] = ""
+
+    venv = virtualenv_create(libexec, "python3")
+
+    resource("PyMuPDF").stage do
+      system libexec/"bin/python3", *Language::Python.setup_install_args(libexec), "build"
+    end
+
+    venv.pip_install resources.reject { |r| r.name == "PyMuPDF" }
+    venv.pip_install_and_link buildpath
   end
 
   test do
